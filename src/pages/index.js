36c457a9f55dd8f80748deadae5aa2192cb1620b
unknown <yeasin.mohammod018@gmail.com>
2023-08-11 11:11:51 +0600

    Title changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import Head from "next/head";
 const Page = () => (
   <>
     <Head>
-      <title>Home - Ismam | Code Segment</title>
+      <title>Home | Code Segment</title>
     </Head>
     <header id="header" class="fixed-top d-flex align-items-center header-transparent">
     <div class="container d-flex align-items-center justify-content-between">
 
       <div class="logo">
-        <h1><a href="index.html"><span>Bootslander</span></a></h1>
+        <h1><a href="index.html"><span>Code Segment</span></a></h1>
         {/* <!-- Uncomment below if you prefer to use an image logo --> */}
         {/* <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>--> */}
       </div>
